ba6d814fe7d5abf3f12aa1b43fbb52c25076253a
Kevin <kersv@Kevins-MBP.fios-router.home>
2022-03-15 14:39:58 -0400

    myMap

--- javascript-methods.js
+++ javascript-methods.js
@@ -7,46 +7,11 @@ Array.prototype.myEach = function(callbackFn) {
   };
   
   // MAP //
-  Array.prototype.myMap = function() {
-    // Place your code here.
-  };
-  
-  // SOME //
-  Array.prototype.mySome = function() {
-    // Place your code here.
-  };
-  
-  // REDUCE //
-  Array.prototype.myReduce = function() {
-    // Place your code here.
-  };
-  
-  // INCLUDES //
-  Array.prototype.myIncludes = function() {
-    // Place your code here.
-  };
-  
-  // INDEXOF //
-  Array.prototype.myIndexOf = function() {
-    // Place your code here.
-  };
-  
-  // PUSH //
-  Array.prototype.myPush = function() {
-    // Place your code here.
-  };
-  
-  // LASTINDEXOF //
-  Array.prototype.myLastIndexOf = function() {
-    // Place your code here.
-  };
-  
-  // KEYS //
-  Object.myKeys = function() {
-    // Place your code here.
-  };
-  
-  // VALUES //
-  Object.myValues = function() {
-    // Place your code here.
+  Array.prototype.myMap = function(callback) {
+    const new_array = [];
+    for(let i = 0; i < this.length; i++){
+        var temp = callback(this[i], i, this);
+        new_array[i] = temp;
+    }
+    return new_array;
   };
